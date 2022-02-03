81d8725f8c6eb8908eb01e7a874555e654c92c13
Hamza Ahmed <dev.hamxa@gmail.com>
2022-02-03 11:28:35 +0100

    Created Commons

---
+++ SAP/SAP/Source/Common/Constant.swift
@@ -0,0 +1,19 @@
+//
+//  Constant.swift
+//  SAPTest
+//
+//  Created by hamza Ahmed on 30.01.22.
+//
+
+import Foundation
+
+class Constant {
+    static let baseUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&"
+    static let apiKey = "87a1c26188f58cf94f6213b4014497ef"
+    static let historyArrayKey = "history"
+    static let storyboardIdentifier = "Main"
+    static let identifierForHistoryTableCell = "HistoryTableViewCell"
+    static let identifierForPhotoCollectionCell = "PhotoCollectionViewCell"
+    static let identifierForHistoryTable = "HistoryTableViewController"
+    
+}
---
+++ SAP/SAP/Source/Common/Extention.swift
@@ -0,0 +1,23 @@
+//
+//  Extention.swift
+//  SAPTest
+//
+//  Created by Hamza Ahmed on 01/02/2022.
+//
+
+import Foundation
+
+extension UserDefaults {
+    
+    func saveHistory(_ historyArr: [History]) {
+        let data = historyArr.map { try? JSONEncoder().encode($0) }
+        UserDefaults.standard.set(data, forKey: Constant.historyArrayKey)
+    }
+    
+    func loadHistory() -> [History] {
+        guard let encodedData = UserDefaults.standard.array(forKey: Constant.historyArrayKey) as? [Data] else {
+            return []
+        }
+        return encodedData.map { try! JSONDecoder().decode(History.self, from: $0) }
+    }
+}
